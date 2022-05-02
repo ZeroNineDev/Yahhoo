26a198f75d3e96e547153a24f74b35921bf9b349
KonovalovIly <xakir0987@gmail.com>
2022-05-02 21:30:30 +0400

    F001: Перевод на gradleKts
    Вынес зависимости в отдельный модуль

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,15 @@
+
+
+object Config {
+
+    const val compileSdk = 32
+    const val targetSdk = 32
+    const val minSdk = 23
+
+    const val applicationId = "com.zeroninedev.yahhoo"
+    const val versionName = "1.0"
+    const val versionCode = 1
+
+    const val jvmTarget = "1.8"
+
+}
